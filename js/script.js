5f15002aadf65663bf8e891fd0fd26e23e2f7cc5
ria <riamrane@gmail.com>
2023-08-20 21:28:38 +0530

    added ecommerce project

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const productList = document.querySelector('.productList');
+
+
+const displayProducts = async () => {
+    const products = await fetchProducts();
+    products.forEach((productData)=>{
+        const product = createProduct(productData);
+        productList.appendChild(product);
+    })
+}
+
+function createProduct(productData){
+    const product = document.createElement('div');
+    product.className = 'product'; 
+    
+    const productImage = document.createElement('div');
+    productImage.className = "productImage";
+
+    const productDetails = document.createElement('div');
+    productDetails.className = "productDetails";
+
+    const  img = document.createElement('img');
+    img.setAttribute("src", productData.thumbnail);
+    img.setAttribute("height", "100%");
+    img.setAttribute("width", "100%");
+
+    productImage.appendChild(img);
+
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const h5 = document.createElement("h5");
+    const span = document.createElement("span");
+
+    h3.innerText = productData.title;
+    p.innerText = productData.description;
+    span.innerText = productData.rating;
+    h5.innerText = "₹" + productData.price;
+
+    productDetails.append(h3);
+    productDetails.append(p);
+    productDetails.append(span);
+    productDetails.append(h5);
+
+    product.appendChild(productImage);
+    product.appendChild(productDetails);
+
+    return product;
+
+}
+
+const fetchProducts = async () => {
+    const response = await fetch('https://dummyjson.com/products');
+    const productsJSON = await response.json();
+    return productsJSON.products;
+}
+
+displayProducts();
+
